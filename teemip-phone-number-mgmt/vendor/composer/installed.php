340306527065a321df4cc1ada4f740451db05b46
xtophe38 <christophe.naud@gmail.com>
2025-01-09 15:08:27 +0100

    Install composer

---
+++ teemip-phone-number-mgmt/vendor/composer/installed.php
@@ -0,0 +1,29 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e88eaed70c433ce9f49cfa05d9e5156466bf9c6e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e88eaed70c433ce9f49cfa05d9e5156466bf9c6e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
+/*
+ * @copyright   Copyright (C) 2010-2025 TeemIp
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+
